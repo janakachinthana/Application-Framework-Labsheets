d9f8e3fbfa5099996064e2cd7b3bc46b78f9df00
JanakaChinthana <janakachinthana1@gmail.com>
2021-04-20 20:08:33 +0530

    labsheet6 completed

--- lab_6/server.js
+++ lab_6/server.js
@@ -5,20 +5,24 @@ const PostRoutes = require('./routes/post.routes');
 const dal = require('./dal');
 
 const app = new koa();
-
+const serve = require('koa-static');
 
 app.use(bodyparser());
-app.use(HomeRoutes.routes())
-    .use(HomeRoutes.allowedMethods());
+
+app.use(serve('public/'));
+
+app.use(HomeRoutes.routes()).use(HomeRoutes.allowedMethods());
 
 app.use(bodyparser());
 app.use(PostRoutes.routes())
     .use(PostRoutes.allowedMethods());
 
-
 app.use(ctx =>{
     ctx.body = 'Hello World';
 });
+
+
+
 app.listen(3000);
 console.log('Application Running on port 3000');
 
